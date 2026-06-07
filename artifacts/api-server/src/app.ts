1b7a56f4e9f0e1e44a6097aca9d858a3ca98df44
Sandeep <sandeep@Naveens-MacBook-Pro.local>
2026-06-08 01:22:55 +0530

    Fix pino-http import

--- artifacts/api-server/src/app.ts
+++ artifacts/api-server/src/app.ts
@@ -1,6 +1,6 @@
 import express, { type Express } from "express";
 import cors from "cors";
-import pinoHttp from "pino-http";
+import pinoHttp = require("pino-http");
 import { clerkMiddleware } from "@clerk/express";
 import { publishableKeyFromHost } from "@clerk/shared/keys";
 import {
